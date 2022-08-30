08b2e6386d24029022468c8a6f9f53c49ab680b5
k.hisomov <k.hisomov@bastion-tech.ru>
2022-08-30 21:35:41 +0300

    set up swagger

--- app/app_core/settings.py
+++ app/app_core/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "broker.apps.BrokerConfig",
     "rest_framework",
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- app/broker/serializers.py
+++ app/broker/serializers.py
@@ -4,6 +4,6 @@ from rest_framework import serializers
 
 class BrokerSerializer(serializers.ModelSerializer):
 
-    class meta:
+    class Meta:
         model = models.Broker
         fields = ['id', 'title', 'description']
