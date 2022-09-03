273050f77e61a51e831c9923dda93e977236a118
k.hisomov <k.hisomov@bastion-tech.ru>
2022-09-03 20:34:51 +0300

    user branch

--- app/broker/models.py
+++ app/broker/models.py
@@ -2,6 +2,8 @@ from django.db import models
 import uuid
 
 
+
+
 class Shares(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=255)
@@ -12,7 +14,7 @@ class Shares(models.Model):
     def __str__(self):
         return self.title
 
-# Create your models here.
+
 class Broker(models.Model):
     title = models.CharField(max_length=250)
     description = models.CharField(max_length=250)
